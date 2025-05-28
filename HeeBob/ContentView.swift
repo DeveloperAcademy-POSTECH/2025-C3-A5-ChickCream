bde6bb2f478e29f64071bf71c169b128444b918f
0tak2 <0tak2.code@gmail.com>
2025-05-28 21:37:36 +0900

    feat: set project

---
+++ HeeBob/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeeBob
+//
+//  Created by 임영택 on 5/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeeBobTests/HeeBobTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeeBobTests.swift
+//  HeeBobTests
+//
+//  Created by 임영택 on 5/28/25.
+//
+
+import Testing
+@testable import HeeBob
+
+struct HeeBobTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
