49c90980d67aa8f1145d306c20a43a62003ad00b
0tak2 <0tak2.code@gmail.com>
2025-05-28 21:37:36 +0900

    feat: set project

---
+++ HeeBob/HeeBobApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeeBobApp.swift
+//  HeeBob
+//
+//  Created by 임영택 on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct HeeBobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
