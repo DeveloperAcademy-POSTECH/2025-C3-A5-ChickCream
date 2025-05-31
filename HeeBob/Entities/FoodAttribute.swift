25ba098c2f953976e50def4dc10751649d284367
0tak2 <0tak2.code@gmail.com>
2025-06-01 01:01:42 +0900

    feat: mainIngredient enum rawValue를 직접 저장

--- HeeBob/Entities/FoodAttribute.swift
+++ HeeBob/Entities/FoodAttribute.swift
@@ -12,19 +12,28 @@ import SwiftData
     @Attribute(.unique) var id: UUID // should be equal to Food entity's id
     @Attribute var isPortable: Bool
     @Attribute var isCookable: Bool
-    @Attribute var mainIngredient: FoodIngredient
+    var mainIngredient: FoodIngredient {
+        get {
+            .init(rawValue: _mainIngredient) ?? .egg
+        }
+        
+        set {
+            _mainIngredient = newValue.rawValue
+        }
+    }
+    @Attribute var _mainIngredient: Int // see: https://stackoverflow.com/a/79294430
     
     init(id: UUID, isPortable: Bool, isCookable: Bool, mainIngredient: FoodIngredient) {
         self.id = id
         self.isPortable = isPortable
         self.isCookable = isCookable
-        self.mainIngredient = mainIngredient
+        self._mainIngredient = mainIngredient.rawValue
     }
     
-    enum FoodIngredient: Codable {
-        case meat
-        case fish
-        case egg
-        case tofu
+    enum FoodIngredient: Int, Codable {
+        case meat = 1
+        case fish = 2
+        case egg = 3
+        case tofu = 4
     }
 }
