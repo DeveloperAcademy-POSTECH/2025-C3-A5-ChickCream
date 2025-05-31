b97d8fff1d8de2c179ab548d749da9c3b5b0189a
h22jun <163622666+h22jun@users.noreply.github.com>
2025-05-31 16:53:51 +0900

    wip

--- HeeBob/Entities/FoodAttribute.swift
+++ HeeBob/Entities/FoodAttribute.swift
@@ -23,10 +23,38 @@ import SwiftData
     }
     @Attribute var _mainIngredient: Int // see: https://stackoverflow.com/a/79294430
     
+    var descriptions: [String] {
+        return [
+            isPortable ? "휴대성이 간편해요" : "휴대못해흥",
+            isCookable ? "소화성" : "",
+            "\(mainIngredient.getDescription())이 들어갔어요",
+        ]
+    }
+    
     init(id: UUID, isPortable: Bool, isCookable: Bool, mainIngredient: FoodIngredient) {
         self.id = id
         self.isPortable = isPortable
         self.isCookable = isCookable
-        self._mainIngredient = mainIngredient.rawValue
+        self.mainIngredient = mainIngredient
+    }
+    
+    enum FoodIngredient: Codable {
+        case meat
+        case fish
+        case egg
+        case tofu
+        
+        func getDescription() -> String {
+            switch self {
+            case .meat:
+                return "살림"
+            case .fish:
+                return "생선"
+            case .egg:
+                return "계란"
+            case .tofu:
+                return "두부"
+            }
+        }
     }
 }
--- HeeBob/MenuDetail/MenuDetailView.swift
+++ HeeBob/MenuDetail/MenuDetailView.swift
@@ -19,28 +19,50 @@ struct MenuDetailView: View {
         }
     }
     
+    var mainComment: AttributedString {
+        var string = AttributedString("여름에는 먹고 겨울에는 안먹음")
+      if let this = string.range(of: "겨울에는 안먹음") {
+        string[this].font = .title3.weight(.bold)
+        string[this].backgroundColor = .red
+      }
+      return string
+    }
+    //TODO: Enum 써서 각각 분류해버리면 될 듯?
+    
+//    func makeHighlightedComment() -> AttributedString {
+//        var string = AttributedString("여름에는 먹고 겨울에는 안먹음")
+//        if let range = string.range(of: "겨울에는 안먹음") {
+//            string[range].font = .title3.weight(.bold)
+//            string[range].backgroundColor = .red
+//        }
+//        return string
+//    }
+
+    
     var body: some View {
         VStack {
             Rectangle()
             .foregroundColor(.clear)
-            .frame(width: 353, height: 353)
+            .frame(maxWidth: .infinity, maxHeight: .infinity , alignment: .topLeading)
             .background(Color(red: 0.85, green: 0.85, blue: 0.85))
-            .cornerRadius(15)
+            .cornerRadius(16)
+            .padding(.horizontal, 20)
             .overlay(Text("Menu Detail Img"))
             
             VStack {
                 Triangle()
                     .fill(Color(red: 1, green: 0.86, blue: 0.8))
-                    .frame(width: 15, height: 15)
+                    .frame(width: 20, height: 15)
                     .padding(.bottom, -10)
                     .padding(.trailing, 280)
-                RoundedRectangle(cornerRadius: 16)
+                Rectangle()
                     .foregroundColor(.clear)
-                    .frame(width: 353, height: 66)
+                    .frame(maxWidth: .infinity, maxHeight: 100, alignment: .topLeading)
                     .background(Color(red: 1, green: 0.86, blue: 0.8))
+                    .cornerRadius(16)
                     .padding(.top, 0)
                     .padding(.bottom, 0)
-                    .cornerRadius(16)
+                    .padding(.horizontal, 20)
                     .overlay(
                         Text("여름에는 덥게 겨울에는 춥게")
                             .font(.title)
@@ -51,18 +73,32 @@ struct MenuDetailView: View {
             RoundedRectangle(cornerRadius: 16)
                 .strokeBorder(Color.gray, lineWidth: 1)
                 .foregroundColor(.clear)
-                .frame(width: 353, height: 145)
+                .frame(maxWidth: .infinity,maxHeight: 200, alignment: .topLeading)
                 .padding(.top, 16)
+                .padding(.horizontal, 20)
                 .overlay(
-                    Text("여름에는 먹고 겨울에는 안 먹고 오이는 절대 안먹고")
-                        .font(.title)
-                        .foregroundColor(.black)
+                    VStack {
+                        HStack {
+                            Image(systemName: "checkmark")
+                            Text(mainComment)}
+                        .padding(.bottom, 4)
+                        .padding(.top, 21)
+                        HStack {
+                            Image(systemName: "checkmark")
+                            Text(mainComment)}
+                        .padding(.bottom, 4)
+                        .padding(.top, 4)
+                        HStack {
+                            Image(systemName: "checkmark")
+                            Text(mainComment)}
+                        .padding(.top, 4)
+                    }
                 )
-            
+                    
             Button(action: { print("찜 삭제, 모달 띄울거임") },
                    label: { Text("찜에서 삭제하기") }
             )
-            .padding(.top, 51)
+            .padding()
             .font(.title2)
             .foregroundStyle(.gray)
         }
