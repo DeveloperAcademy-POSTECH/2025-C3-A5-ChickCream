0b5f11f302cfca6c7847ad1094953d7dc8e98c07
BoMin <bmlee@postech.ac.kr>
2025-06-01 17:16:02 +0900

    feat: 결과 보기 뷰

--- HeeBob/Results/ResultsView.swift
+++ HeeBob/Results/ResultsView.swift
@@ -8,12 +8,49 @@
 import SwiftUI
 
 struct ResultsView: View {
+    @State private var activeID: String?
+    let carouselItems: [CarouselItem] = foods.map { .food($0) } + [.addCard]
+    
     var body: some View {
         VStack {
-            Text("ResultsView")
+            ResultsCarousel(config: .init(hasScale: true, cardWidth: UIScreen.main.bounds.width*0.81),
+                            selection: $activeID,
+                            data: carouselItems) { item in
+                switch item {
+                case .food(let food):
+                    ResultCard(food: food) {
+                        print(food.title)
+                    }
+                case .addCard:
+                    AddCard {
+                        print("아이템 추가 누름")
+                    }
+                }
+            }
+            .frame(height: UIScreen.main.bounds.height*0.4)
+            
+            VStack {
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Text("찜한 메뉴")
+                    }
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Text("홈")
+                    }
+
+                }
+                .padding()
+            }
         }
     }
 }
+
+
 
 #Preview {
     ResultsView()
