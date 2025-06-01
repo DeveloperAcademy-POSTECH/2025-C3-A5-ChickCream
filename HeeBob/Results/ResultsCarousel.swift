bc7af5bb9ecf0ea536d92c384e4bb53ec053c642
Minbol <bmlee@postech.ac.kr>
2025-06-01 17:27:42 +0900

    chore: Carousel 원본 코드 삭제

--- HeeBob/Results/ResultsCarousel.swift
+++ HeeBob/Results/ResultsCarousel.swift
@@ -54,43 +54,6 @@ struct ResultsCarousel<Content: View, Data: RandomAccessCollection>: View where 
         }
         .frame(width: config.cardWidth)
     }
-
-/// Carousel 원본 코드
-//    func ItemView(_ item: Data.Element) -> some View {
-//        GeometryReader { proxy in
-//            let size = proxy.size
-//            
-//            let minX = proxy.frame(in: .scrollView(axis: .horizontal)).minX
-//            let progress = minX / (config.cardWidth + config.spacing)
-//            let minimumCardWidth = config.minimumCardWidth
-//            
-//            let diffWidth = config.cardWidth - minimumCardWidth
-//            let reducingWidth = diffWidth * progress
-//            /// Limiting diffWidth as the Max Value
-//            let cappedWidth = min(reducingWidth, diffWidth)
-//            
-//            let resizedFrameWidth = size.width - (minX > 0 ? cappedWidth : min(-cappedWidth, diffWidth))
-//            let negativeProgress = max(-progress, 0)
-//            
-//            let scaleValue = config.scaleValue * abs(progress)
-//            
-//            content(item)
-//                .frame(width: size.width, height: size.height)
-//                .frame(width: resizedFrameWidth)
-//                .scaleEffect(config.hasScale ? 1 - scaleValue : 1)
-//                .mask {
-//                    let hasScale = config.hasScale
-//                    let scaledHeight = (1 - scaleValue) * size.height
-//                    RoundedRectangle(cornerRadius: config.cornerRadius)
-//                        .frame(height: hasScale ? max(scaledHeight, 0) : size.height)
-//                }
-//                .clipShape(.rect(cornerRadius: config.cornerRadius))
-//                .offset(x: -reducingWidth)
-//                .offset(x: min(progress, 1) * diffWidth)
-//                .offset(x: negativeProgress * diffWidth)
-//        }
-//        .frame(width: config.cardWidth)
-//    }
     
     struct Config {
         var hasScale: Bool = true
