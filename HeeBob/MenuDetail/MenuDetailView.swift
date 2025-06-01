f7a06a5619418b0d5e797bbaca4fd37d50c239af
h22jun <163622666+h22jun@users.noreply.github.com>
2025-06-01 23:06:57 +0900

    feat/rebase 후 UserAnswerView를 DetailView에 적용

--- HeeBob/MenuDetail/MenuDetailView.swift
+++ HeeBob/MenuDetail/MenuDetailView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 import SwiftData
 
+
 struct MenuDetailView: View {
     var food: Food
-    var favorite: Favorite
+//    var favorite: Favorite
     @Environment(\.modelContext) private var modelContext
     
     struct Triangle: Shape {
@@ -27,14 +28,14 @@ struct MenuDetailView: View {
     var body: some View {
         VStack {
             Rectangle()
-            .foregroundColor(.clear)
-            .frame(maxHeight: 361 , alignment: .topLeading)
-            .background(Color.gray)
-            .cornerRadius(16)
-            .padding(.top, 0)
-            .padding(.horizontal, 16)
-            .padding(.bottom, 0)
-            .overlay(Text("Menu Detail Img"))
+                .foregroundColor(.clear)
+                .frame(maxHeight: 361 , alignment: .topLeading)
+                .background(Color.gray)
+                .cornerRadius(16)
+                .padding(.top, 0)
+                .padding(.horizontal, 16)
+                .padding(.bottom, 0)
+                .overlay(Text("Menu Detail Img"))
             
             VStack {
                 Triangle()
@@ -60,33 +61,53 @@ struct MenuDetailView: View {
                             .fixedSize(horizontal: false, vertical: true) //2줄 이상 가능하게
                             .frame(alignment: .leading)
                     }
-            }
+                }
             
-            RoundedRectangle(cornerRadius: 16)
-                .strokeBorder(Color.gray, lineWidth: 1)
-                .foregroundColor(.clear)
-                .frame(maxWidth: .infinity,maxHeight: 148, alignment: .topLeading)
-                .padding(.top, 16)
-                .padding(.horizontal, 16)
-                .overlay(
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image(systemName: "checkmark")
-                            Text(" 대충 휴대성 설명")}
-                        .padding(.bottom, 4)
-                        .padding(.top, 21)
-                        HStack {
-                            Image(systemName: "checkmark")
-                            Text(" 대충 요리해먹기 설명")}
-                        .padding(.bottom, 4)
-                        .padding(.top, 4)
-                        HStack {
-                            Image(systemName: "checkmark")
-                            Text(" 대충 주재료 설명")}
-                        .padding(.top, 4)
-                    }
-                        .padding(.trailing, 150)
-                )
+            UserAnswerView(
+                userAnswer: .init(isPortable: true, isCookable: false, mainIngredient: .meat),
+                borderColor: .hbButtonSecondary,
+                backgroundColor: .clear
+            )
+            .padding()
+            
+            Button(action: {
+                print("찜 삭제, 모달 띄울거임")
+            },
+                   label: { Text("찜에서 삭제하기/찜하기") }
+            )
+            .padding(.top, 40)
+            .font(.hbSubtitle)
+            .foregroundStyle(Color.hbDisabled)
+        }
+    }
+}
+                   
+
+//            RoundedRectangle(cornerRadius: 16)
+//                .strokeBorder(Color.gray, lineWidth: 1)
+//                .foregroundColor(.clear)
+//                .frame(maxWidth: .infinity,maxHeight: 148, alignment: .topLeading)
+//                .padding(.top, 16)
+//                .padding(.horizontal, 16)
+//                .overlay(
+//                    VStack(alignment: .leading) {
+//                        HStack {
+//                            Image(systemName: "checkmark")
+//                            Text(" 대충 휴대성 설명")}
+//                        .padding(.bottom, 4)
+//                        .padding(.top, 21)
+//                        HStack {
+//                            Image(systemName: "checkmark")
+//                            Text(" 대충 요리해먹기 설명")}
+//                        .padding(.bottom, 4)
+//                        .padding(.top, 4)
+//                        HStack {
+//                            Image(systemName: "checkmark")
+//                            Text(" 대충 주재료 설명")}
+//                        .padding(.top, 4)
+//                    }
+//                        .padding(.trailing, 150)
+//                )
                     
 //            
 //            해당 메뉴가 뻬이보릿이면
@@ -111,18 +132,8 @@ struct MenuDetailView: View {
 //        .foregroundStyle(Color.hbDisabled)
 // if문 쓰는게 더 깔끔할듯? 일단 월요일 회의에서 확정나면 작업할 예정
             
-            Button(action: {
-                print("찜 삭제, 모달 띄울거임")
-            },
-                   label: { Text("찜에서 삭제하기/찜하기") }
-            )
-            .padding(.top, 40)
-            .font(.hbSubtitle)
-            .foregroundStyle(Color.hbDisabled)
-        }
-    }
-}
+
 
 #Preview {
-    MenuDetailView(food: Food(id: UUID(), title: "김치찌개", uniquePoint: "여름에 먹으면 별미에요. 이건 2줄 테스트용인뎅 후후후후~ 3줄은 안되게해야하지않을까요~", author: "aa", attribute: FoodAttribute(id: UUID(), isPortable: true, isCookable: true, mainIngredient: .meat)), favorite: Favorite(id: UUID(), food: Food(id: UUID(), title: "김치찌개", uniquePoint: "먹고싶은 음식", author: "aaa", attribute: FoodAttribute(id: UUID(), isPortable: true, isCookable: true, mainIngredient: .meat)), createdAt: Date()))
+    MenuDetailView(food: .init(id: UUID(), title: "김치찌개", uniquePoint: "새콤달큰매콤", attribute: .init(id: UUID(), isPortable: true, isCookable: true, mainIngredient: .meat)))
 }
