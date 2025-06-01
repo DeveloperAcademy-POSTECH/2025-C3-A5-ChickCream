f691ed2b828fbd147b7a55a021d4d632df190b2c
BoMin <bmlee@postech.ac.kr>
2025-06-01 17:15:53 +0900

    feat: 결과 보기 carousel

--- HeeBob/Results/ResultsCarousel.swift
+++ HeeBob/Results/ResultsCarousel.swift
@@ -21,71 +21,83 @@ struct ResultsCarousel<Content: View, Data: RandomAccessCollection>: View where 
                     ForEach(data) { item in
                         ItemView(item)
                     }
+                   
                 }
                 .scrollTargetLayout()
                 
             }
-            /// Making it to start and end at the center
-            .safeAreaPadding(.horizontal, max((size.width - config.cardWidth) / 2, 0))
-            /// Making it to position on image(card)
-            .scrollPosition(id: $selection)
-            /// Making it a carousel
+            .safeAreaPadding(.horizontal, max((size.width - config.cardWidth) / 2, 0)) // 중앙에서 시작하고 끝내기 위함
+            .scrollPosition(id: $selection) // Position 고정하기 위함
             .scrollTargetBehavior(.viewAligned(limitBehavior: .always))
-            /// Hiding Scroll Indicator
             .scrollIndicators(.hidden)
         }
     }
     /// Item View
     @ViewBuilder
     func ItemView(_ item: Data.Element) -> some View {
-        GeometryReader { proxy in
-            let size = proxy.size
+        GeometryReader { proxy in            
+            let frame = proxy.frame(in: .global) // 화면 전체 좌표계 기준
+            let centerX = UIScreen.main.bounds.width / 2
+            let distanceFromCenter = abs(frame.midX - centerX)
             
-            let minX = proxy.frame(in: .scrollView(axis: .horizontal)).minX
-            let progress = minX / (config.cardWidth + config.spacing)
-            let minimumCardWidth = config.minimumCardWidth
-            
-            let diffWidth = config.cardWidth - minimumCardWidth
-            let reducingWidth = diffWidth * progress
-            /// Limiting diffWidth as the Max Value
-            let cappedWidth = min(reducingWidth, diffWidth)
-            
-            let resizedFrameWidth = size.width - (minX > 0 ? cappedWidth : min(-cappedWidth, diffWidth))
-            let negativeProgress = max(-progress, 0)
-            
-            let scaleValue = config.scaleValue * abs(progress)
+            let maxDistance = UIScreen.main.bounds.width / 2 + config.cardWidth
+            let progress = min(distanceFromCenter / maxDistance, 1)
+            let scaleValue = config.scaleValue * progress
+            let scale = config.hasScale ? 1 - scaleValue : 1
             
             content(item)
-                .frame(width: size.width, height: size.height)
-                .frame(width: resizedFrameWidth)
-                .scaleEffect(config.hasScale ? 1 - scaleValue : 1)
-                .mask {
-                    let hasScale = config.hasScale
-                    let scaledHeight = (1 - scaleValue) * size.height
-                    RoundedRectangle(cornerRadius: config.cornerRadius)
-                        .frame(height: hasScale ? max(scaledHeight, 0) :  size.height)
-                }
-                .clipShape(.rect(cornerRadius: config.cornerRadius))
-                .offset(x: -reducingWidth)
-                .offset(x: min(progress, 1) * diffWidth)
-                .offset(x: negativeProgress * diffWidth)
+                .aspectRatio(contentMode: .fit)
+                .scaleEffect(scale)
+                .animation(.easeOut(duration: 0.2), value: scale)
+                .frame(width: config.cardWidth)
+                .clipShape(RoundedRectangle(cornerRadius: config.cornerRadius))
         }
         .frame(width: config.cardWidth)
     }
+
+/// Carousel 원본 코드
+//    func ItemView(_ item: Data.Element) -> some View {
+//        GeometryReader { proxy in
+//            let size = proxy.size
+//            
+//            let minX = proxy.frame(in: .scrollView(axis: .horizontal)).minX
+//            let progress = minX / (config.cardWidth + config.spacing)
+//            let minimumCardWidth = config.minimumCardWidth
+//            
+//            let diffWidth = config.cardWidth - minimumCardWidth
+//            let reducingWidth = diffWidth * progress
+//            /// Limiting diffWidth as the Max Value
+//            let cappedWidth = min(reducingWidth, diffWidth)
+//            
+//            let resizedFrameWidth = size.width - (minX > 0 ? cappedWidth : min(-cappedWidth, diffWidth))
+//            let negativeProgress = max(-progress, 0)
+//            
+//            let scaleValue = config.scaleValue * abs(progress)
+//            
+//            content(item)
+//                .frame(width: size.width, height: size.height)
+//                .frame(width: resizedFrameWidth)
+//                .scaleEffect(config.hasScale ? 1 - scaleValue : 1)
+//                .mask {
+//                    let hasScale = config.hasScale
+//                    let scaledHeight = (1 - scaleValue) * size.height
+//                    RoundedRectangle(cornerRadius: config.cornerRadius)
+//                        .frame(height: hasScale ? max(scaledHeight, 0) : size.height)
+//                }
+//                .clipShape(.rect(cornerRadius: config.cornerRadius))
+//                .offset(x: -reducingWidth)
+//                .offset(x: min(progress, 1) * diffWidth)
+//                .offset(x: negativeProgress * diffWidth)
+//        }
+//        .frame(width: config.cardWidth)
+//    }
     
-    /// Config
     struct Config {
-        var hasScale: Bool = false
+        var hasScale: Bool = true
         var scaleValue: CGFloat = 0.2
         
         var cardWidth: CGFloat = 150
-        var spacing: CGFloat = 10
+        var spacing: CGFloat = 0
         var cornerRadius: CGFloat = 15
-        var minimumCardWidth: CGFloat = 40
-        
     }
-}
-
-#Preview {
-    ContentView()
 }
