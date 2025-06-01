a3191485bdbd261d0d4c77d73e21d439c4ccc76d
0tak2 <0tak2.code@gmail.com>
2025-06-01 14:42:09 +0900

    feat: FoodIngredient를 FoodAttribute 밖에서 관리하고, rawValue를 직접 저장

---
+++ HeeBob/Entities/FoodIngredient.swift
@@ -0,0 +1,15 @@
+//
+//  FoodIngredient.swift
+//  HeeBob
+//
+//  Created by 임영택 on 6/1/25.
+//
+
+import Foundation
+
+enum FoodIngredient: Int, Codable {
+    case meat = 1
+    case fish
+    case egg
+    case tofu
+}
