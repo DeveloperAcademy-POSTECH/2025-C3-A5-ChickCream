61738af13f1a9fe4e6cc529627b417363d3a3c3c
0tak2 <0tak2.code@gmail.com>
2025-06-01 23:42:57 +0900

    wip

---
+++ HeeBob/Question/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  HeeBob
+//
+//  Created by 임영택 on 6/1/25.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
