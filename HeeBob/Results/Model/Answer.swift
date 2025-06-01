753eed12691784e899f124b7a1e2d7030d854273
BoMin <bmlee@postech.ac.kr>
2025-06-01 17:19:10 +0900

    feat: 답변 임시 모델 생성
    - 삭제될 예정

---
+++ HeeBob/Results/Model/Answer.swift
@@ -0,0 +1,20 @@
+//
+//  Answer.swift
+//  HeeBob
+//
+//  Created by BoMin Lee on 5/31/25.
+//
+
+import Foundation
+
+struct ResultModel: Identifiable {
+    var id: UUID = .init()
+    var image: String
+}
+
+// TODO: 삭제 예정
+var foods: [Food] = [
+    Food(id: UUID(), title: "닭갈비", uniquePoint: "아따 맛있다", author: "-", attribute: FoodAttribute(id: UUID(), isPortable: true, isCookable: true, mainIngredient: .meat)),
+    Food(id: UUID(), title: "에그마요 샌디치", uniquePoint: "아따 맛있다", author: "-", attribute: FoodAttribute(id: UUID(), isPortable: true, isCookable: true, mainIngredient: .meat)),
+    Food(id: UUID(), title: "족발", uniquePoint: "아따 맛있다", author: "-", attribute: FoodAttribute(id: UUID(), isPortable: true, isCookable: true, mainIngredient: .meat)),
+]
