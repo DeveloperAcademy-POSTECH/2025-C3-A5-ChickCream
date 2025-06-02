913ca56041a9270e3bee813923b26ecb3c2ed085
BoMin <bmlee@postech.ac.kr>
2025-06-02 16:03:55 +0900

    refactor: Route에서 home 삭제

--- HeeBob/Navigation/Route.swift
+++ HeeBob/Navigation/Route.swift
@@ -10,7 +10,6 @@ import SwiftUI
 /// 단순 이동뿐만 아니라, 화면에 필요한 데이터를 함께 전달 가능
 // FIXME: 각 뷰별로 전달받는 데이터에 따라 수정 필요
 enum Route: Hashable { // Hashable을 채택해야 NavigationPath에서 활용 가능
-    case home
     case question
     case loading
     case result
