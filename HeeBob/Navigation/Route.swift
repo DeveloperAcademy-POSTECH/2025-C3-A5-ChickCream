5aaec19f86aab0d13544210d61c19f2fa9033622
BoMin <bmlee@postech.ac.kr>
2025-06-02 15:39:55 +0900

    chore: 주석 수정

--- HeeBob/Navigation/Route.swift
+++ HeeBob/Navigation/Route.swift
@@ -7,14 +7,12 @@
 
 import SwiftUI
 
-
 /// 단순 이동뿐만 아니라, 화면에 필요한 데이터를 함께 전달 가능
+// FIXME: 각 뷰별로 전달받는 데이터에 따라 수정 필요
 enum Route: Hashable { // Hashable을 채택해야 NavigationPath에서 활용 가능
     case home
     case question
-    case loading // 결과 확인 내부로 뺄지 고민
+    case loading
     case result
     case favorite
 }
-
-
