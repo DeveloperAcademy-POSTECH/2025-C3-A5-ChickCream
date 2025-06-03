36205b670a0d1f4a73f3d4c2fc17916e019d837e
0tak2 <0tak2.code@gmail.com>
2025-06-03 14:38:39 +0900

    chore: 가독성 개선

--- HeeBob/Question/QuestionView.swift
+++ HeeBob/Question/QuestionView.swift
@@ -23,7 +23,11 @@ struct QuestionView: View {
             if viewModel.selectedQuestion.options.count == 2 {
                 HStack(spacing: 16) {
                     ForEach(viewModel.selectedQuestion.options.indices, id: \.self) { index in
-                        QuestionOptionButton(title: viewModel.selectedQuestion.options[index], type: .half, isDisabled: viewModel.selectedQuestion.selectedOptionIndex != index) {
+                        QuestionOptionButton(
+                            title: viewModel.selectedQuestion.options[index],
+                            type: .half,
+                            isDisabled: viewModel.selectedQuestion.selectedOptionIndex != index
+                        ) {
                             viewModel.selectOption(at: index)
                         }
                     }
@@ -33,7 +37,11 @@ struct QuestionView: View {
                 VStack(spacing: 16) {
                     HStack(spacing: 16) {
                         ForEach(viewModel.selectedQuestion.options.indices[0..<2], id: \.self) { index in
-                            QuestionOptionButton(title: viewModel.selectedQuestion.options[index], type: .quarter, isDisabled: viewModel.selectedQuestion.selectedOptionIndex != index) {
+                            QuestionOptionButton(
+                                title: viewModel.selectedQuestion.options[index],
+                                type: .quarter,
+                                isDisabled: viewModel.selectedQuestion.selectedOptionIndex != index
+                            ) {
                                 viewModel.selectOption(at: index)
                             }
                         }
@@ -41,7 +49,11 @@ struct QuestionView: View {
                     
                     HStack(spacing: 16) {
                         ForEach(viewModel.selectedQuestion.options.indices[2..<4], id: \.self) { index in
-                            QuestionOptionButton(title: viewModel.selectedQuestion.options[index], type: .quarter, isDisabled: viewModel.selectedQuestion.selectedOptionIndex != index) {
+                            QuestionOptionButton(
+                                title: viewModel.selectedQuestion.options[index],
+                                type: .quarter,
+                                isDisabled: viewModel.selectedQuestion.selectedOptionIndex != index
+                            ) {
                                 viewModel.selectOption(at: index)
                             }
                         }
@@ -53,11 +65,28 @@ struct QuestionView: View {
             }
             
             HStack {
-                HBButton(configuration: .init(title: "이전", imageName: "chevron.left", imageType: .system, imagePosition: .left, foregroundColor: .hbPrimary, backgroundColor: .hbPrimaryLighten, disabled: viewModel.selectedIndex == 0)) {
+                HBButton(
+                    configuration: .init(
+                        title: "이전",
+                        imageName: "chevron.left",
+                        imageType: .system,
+                        imagePosition: .left,
+                        foregroundColor: .hbPrimary,
+                        backgroundColor: .hbPrimaryLighten,
+                        disabled: viewModel.selectedIndex == 0
+                    )
+                ) {
                     viewModel.showPreviousQuestion()
                 }
                 
-                HBButton(configuration: .init(title: viewModel.selectedIndex == viewModel.questions.count - 1 ? "결과 보기" : "다음", imageName: "chevron.right", imageType: .system, imagePosition: .right, disabled: viewModel.selectedQuestion.selectedOptionIndex == nil)) {
+                HBButton(
+                    configuration: .init(
+                        title: viewModel.selectedIndex == viewModel.questions.count - 1 ? "결과 보기" : "다음",
+                        imageName: "chevron.right",
+                        imageType: .system,
+                        imagePosition: .right,
+                        disabled: viewModel.selectedQuestion.selectedOptionIndex == nil)
+                ) {
                     if viewModel.selectedIndex == viewModel.questions.count - 1 {
                         print(viewModel.synthesizedAnswer) // TODO: UserModel을 결과 로딩 뷰로 넘김
                     } else {
