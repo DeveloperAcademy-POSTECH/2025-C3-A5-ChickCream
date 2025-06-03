76257e83e6d54cfebd92dabf30f9c614070204be
0tak2 <0tak2.code@gmail.com>
2025-06-03 14:38:31 +0900

    refactor: extract sub views

---
+++ HeeBob/Question/QuestionTitleView.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionTitleView.swift
+//  HeeBob
+//
+//  Created by 임영택 on 6/3/25.
+//
+
+import SwiftUI
+
+struct QuestionTitleView: View {
+    let content: String
+    
+    init(_ content: String) {
+        self.content = content
+    }
+    
+    var body: some View {
+        Text(content)
+            .font(.hbTitle)
+            .foregroundStyle(Color.hbTextPrimary)
+            .multilineTextAlignment(.center)
+    }
+}
--- HeeBob/Question/QuestionView.swift
+++ HeeBob/Question/QuestionView.swift
@@ -71,51 +71,6 @@ struct QuestionView: View {
     }
 }
 
-struct QuestionTitleView: View {
-    let content: String
-    
-    init(_ content: String) {
-        self.content = content
-    }
-    
-    var body: some View {
-        Text(content)
-            .font(.hbTitle)
-            .foregroundStyle(Color.hbTextPrimary)
-            .multilineTextAlignment(.center)
-    }
-}
-
-struct QuestionOptionButton: View {
-    let title: String
-    let type: QuestionOptionButtonType
-    let isDisabled: Bool
-    let didTap: () -> Void
-    
-    var body: some View {
-        Button {
-            didTap()
-        } label: {
-            ZStack {
-                RoundedRectangle(cornerRadius: 16)
-                    .fill(isDisabled ? Color.clear : Color.hbPrimaryLighten)
-                    .stroke(isDisabled ? Color.hbDisabled : Color.hbPrimary, style: .init(lineWidth: 3))
-                    .frame(width: 173, height: type == .half ? 160 : 144 )
-                
-                Text(title)
-                    .font(.hbSubtitle)
-                    .foregroundStyle(isDisabled ? Color.hbDisabled : Color.hbPrimary)
-            }
-        }
-
-    }
-    
-    enum QuestionOptionButtonType {
-        case half
-        case quarter
-    }
-}
-
 #Preview {
     QuestionView()
 }
