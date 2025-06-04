2476e67ca79e7904c714d23ed6b425389c48f330
임영택 <0tak2.code@gmail.com>
2025-06-04 20:37:36 +0900

    [Issue #47] 홈 (#48)
    * feat: 홈뷰 레이아웃
    
    * assets: 로티 애니메이션
    
    * style: 하이파이 프로토타입에 맞게 레이아웃 수정
    
    * style: 디자인 디테일 적용
    
    * refactor: 홈 버튼 분리
    
    * feat: 물음표 이미지 버튼으로 변경
    
    * chore: 네비게이팅 필요한 부분에 주석 추가
    
    * style: 앱 제목 컬러 적용
    
    * style: 버튼 타이틀 색상 적용

---
+++ HeeBob/Home/HomeView.swift
@@ -0,0 +1,68 @@
+//
+//  HomeView.swift
+//  HeeBob
+//
+//  Created by 임영택 on 6/3/25.
+//
+
+import SwiftUI
+import Lottie
+
+struct HomeView: View {
+    let onboardingButtonColor = Color(hex: "#1C1B1F")
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                stops: [
+                    .init(color: .init(hex: "#F9AC80"), location: 0.0),
+                    .init(color: .init(hex: "#FF6933"), location: 1.0),
+                ],
+                startPoint: UnitPoint(x: 0.33, y: 0),
+                endPoint: UnitPoint(x: 0.66, y: 1.0)
+            )
+            .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 16) {
+                HStack {
+                    Text("희밥")
+                        .font(.hbTitle)
+                        .foregroundStyle(Color.hbTextPrimary)
+                    
+                    Spacer()
+                    
+                    Button {
+                        print("온보딩 보기") // TODO: Navigate
+                    } label: {
+                        Image(systemName: "questionmark.circle")
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundStyle(onboardingButtonColor)
+                            .frame(width: 24, height: 24)
+                    }
+                }
+                .padding(24)
+                
+                LottieView(animation: .named("home"))
+                    .playing(loopMode: .loop)
+                    .frame(maxHeight: .infinity)
+                    .padding(.horizontal, 36)
+                
+                VStack(spacing: 16) {
+                    HomeButton(title: "메뉴 추천받기") {
+                        print("메뉴 추천") // TODO: Navigate
+                    }
+                    
+                    HomeButton(title: "찜한 메뉴 보러가기") {
+                        print("찜한 메뉴") // TODO: Navigate
+                    }
+                }
+                .padding(24)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
