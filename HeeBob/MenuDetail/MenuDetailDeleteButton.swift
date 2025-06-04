ffad7dd1f8b3a8203ed6642561f0f1d2551e02c5
h22jun <163622666+h22jun@users.noreply.github.com>
2025-06-04 14:54:23 +0900

    [Feat #14] 메뉴 상세보기 뷰 구현 (#19)
    * docs: TEST.md 추가
    
    * feat: MenuDetailView 기본 틀 작성
    
    * wip
    
    * feat : description, 강조 표현 작성
    
    * feat: padding, font, color 수정 및 description에 글꼴 적용 안되는 것 수정
    
    * feat: 피드백 반영하여 수정 및 description뷰모델로 변경 진행 중
    
    * chore: 변경점 Rebase
    
    * feat/데이터 연결 작업 중
    
    * feat/텍스트 좌측 정렬 안되던 이슈 해결 및 메뉴의 찜상태에 따라 버튼 달라지는 것 미리 테스트 구현
    
    * feat/rebase 후 UserAnswerView를 DetailView에 적용
    
    * feat: 상세보기 뷰 구현 및 데이터 연결, 약간의 리펙토링
    
    * lint: 불필요한 코드 삭제
    
    * lint: 코드 가독성 향상
    
    * lint: 코드 피드백 반영
    
    * lint: alert 메시지 수정
    
    * chore: project.pbxproj

---
+++ HeeBob/MenuDetail/MenuDetailDeleteButton.swift
@@ -0,0 +1,30 @@
+//
+//  MenuDetailDeleteButton.swift
+//  HeeBob
+//
+//  Created by 산들 on 6/4/25.
+//
+
+import SwiftUI
+
+struct MenuDetailDeleteButton: View {
+    var food: Food
+    @Environment(\.modelContext) private var modelContext
+    @State private var showFavoriteDeleteAlert = false
+    
+    var body: some View {
+        Button("찜에서 삭제하기") {
+            showFavoriteDeleteAlert = true
+        }
+        .alert("정말 삭제 하시겠습니까?", isPresented: $showFavoriteDeleteAlert) {
+            Button("삭제", role: .destructive) {
+                modelContext.delete(food)
+            }
+            Button("취소", role: .cancel) { }
+        }
+        .padding(.top, 40)
+        .font(.hbSubtitle)
+        .foregroundStyle(Color.hbDisabled)
+    }
+}
+
---
+++ HeeBob/MenuDetail/MenuDetailView.swift
@@ -0,0 +1,35 @@
+//
+//  MenuDetailView.swift
+//  HeeBob
+//
+//  Created by 산들 on 5/31/25.
+//
+import SwiftUI
+import SwiftData
+
+
+struct MenuDetailView: View {
+    var food: Food
+    
+    var body: some View {
+        
+        VStack {
+            MenuDetailImageBox(food: food)
+            
+            MenuDetailUniquePointBox(food: food)
+            
+            UserAnswerView(
+                userAnswer: .init(isPortable: true, isCookable: false, mainIngredient: .meat),
+                borderColor: .hbButtonSecondary,
+                backgroundColor: .clear
+            )
+            .padding()
+            
+            MenuDetailDeleteButton(food: food)
+        }
+    }
+}
+
+#Preview {
+    MenuDetailView(food: .init(id: UUID(uuidString: "b9fbc325-de04-48a7-805b-000000000000")!, title: "김치찌개", uniquePoint: "새콤달큰매콤새콤달큰매콤새콤달큰매콤새콤달큰매콤새콤달큰매콤새콤달큰매콤새콤달큰", attribute: .init(id: UUID(), isPortable: true, isCookable: true, mainIngredient: .meat)))
+}
