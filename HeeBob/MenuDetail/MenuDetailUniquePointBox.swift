00ade7c51774c9ef54d7ee99fb96a1d97c57d863
h22jun <163622666+h22jun@users.noreply.github.com>
2025-06-04 14:54:23 +0900

    [Feat #14] 메뉴 상세보기 뷰 구현 (#19)
    * docs: TEST.md 추가
    
    * feat: MenuDetailView 기본 틀 작성
    
    * wip
    
    * feat : description, 강조 표현 작성
    
    * feat: padding, font, color 수정 및 description에 글꼴 적용 안되는 것 수정
    
    * feat: 피드백 반영하여 수정 및 description뷰모델로 변경 진행 중
    
    * chore: 변경점 Rebase
    
    * feat/데이터 연결 작업 중
    
    * feat/텍스트 좌측 정렬 안되던 이슈 해결 및 메뉴의 찜상태에 따라 버튼 달라지는 것 미리 테스트 구현
    
    * feat/rebase 후 UserAnswerView를 DetailView에 적용
    
    * feat: 상세보기 뷰 구현 및 데이터 연결, 약간의 리펙토링
    
    * lint: 불필요한 코드 삭제
    
    * lint: 코드 가독성 향상
    
    * lint: 코드 피드백 반영
    
    * lint: alert 메시지 수정
    
    * chore: project.pbxproj

---
+++ HeeBob/MenuDetail/MenuDetailUniquePointBox.swift
@@ -0,0 +1,51 @@
+//
+//  MenuDetailUniquePointBox.swift
+//  HeeBob
+//
+//  Created by 산들 on 6/4/25.
+//
+
+import SwiftUI
+
+struct MenuDetailUniquePointBox: View {
+    var food: Food
+    
+    var body: some View {
+        VStack {
+            Triangle()
+                .fill(Color(red: 1, green: 0.86, blue: 0.8))
+                .frame(width: 20, height: 15)
+                .padding(.bottom, -10)
+                .padding(.top, 0)
+                .padding(.trailing, 300)
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(maxHeight: 66, alignment: .topLeading)
+                .background(Color.hbPrimaryLighten)
+                .cornerRadius(16)
+                .padding(.top, 0)
+                .padding(.bottom, 0)
+                .padding(.horizontal, 16)
+                .overlay(alignment: .leading) {
+                    Text(food.uniquePoint)
+                        .font(.hbBody1)
+                        .foregroundColor(.black)
+                        .padding(16)
+                        .padding(.leading, 14)
+                        .fixedSize(horizontal: false, vertical: true) // 2줄 이상 가능하게
+                        .frame(alignment: .leading)
+                }
+        }
+    }
+    
+    struct Triangle: Shape {
+        func path(in rect: CGRect) -> Path {
+            Path { path in
+                path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+                path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+                path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+                path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+            }
+        }
+    }
+}
