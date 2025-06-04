3cdbb92d7c06adbe7494ee673779e2dfe9720a90
0tak2 <0tak2.code@gmail.com>
2025-06-04 15:22:20 +0900

    style: 하이파이 프로토타입에 맞게 레이아웃 수정

--- HeeBob/Home/HomeView.swift
+++ HeeBob/Home/HomeView.swift
@@ -6,33 +6,51 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct HomeView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            HStack {
-                Text("희밥")
-                    .font(.hbTitle)
+        ZStack {
+            LinearGradient(
+                stops: [
+                    .init(color: .init(hex: "#F9AC80"), location: 0.0),
+                    .init(color: .init(hex: "#FF6933"), location: 1.0),
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 16) {
+                HStack {
+                    Text("희밥")
+                        .font(.hbTitle)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "questionmark.circle")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                }
+                .padding(24)
                 
-                Spacer()
-            }
-            
-            Text("오직 당신만을 위한\n메뉴로 골라드려요")
-                .multilineTextAlignment(.leading)
-                .font(.suite(type: .extraBold, size: 36))
-            
-            Spacer()
-            
-            HomeButton(title: "메뉴 추천받기") {
-                print("메뉴 추천")
-            }
-
-            HomeButton(title: "찜한 메뉴 보러가기") {
-                print("찜한 메뉴")
+                LottieView(animation: .named("home"))
+                    .playing(loopMode: .loop)
+                    .frame(maxHeight: .infinity)
+                    .padding(.horizontal, 36)
+                
+                VStack(spacing: 16) {
+                    HomeButton(title: "메뉴 추천받기") {
+                        print("메뉴 추천")
+                    }
+                    
+                    HomeButton(title: "찜한 메뉴 보러가기") {
+                        print("찜한 메뉴")
+                    }
+                }
+                .padding(24)
             }
         }
-        .frame(maxWidth: .infinity)
-        .padding(24)
     }
 }
 
@@ -40,17 +58,19 @@ struct HomeButton: View {
     let title: String
     let didTap: () -> Void
     
+    let titleColor = Color(hex: "#000000")
+    
     var body: some View {
         Button {
             didTap()
         } label: {
             ZStack {
-                Capsule()
-                    .fill(Color.hbPrimary)
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.hbPrimaryLighten)
                 
                 Text(title)
-                    .foregroundStyle(Color.hbWhite)
-                    .font(.hbTitle)
+                    .foregroundStyle(titleColor)
+                    .font(.hbSubtitle)
             }
         }
         .frame(height: 90)
