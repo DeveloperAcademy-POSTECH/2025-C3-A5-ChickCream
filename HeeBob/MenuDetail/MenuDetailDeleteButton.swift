aae98fa9f7b74aa4cd468ebfdf1101e9cb79772c
0tak2 <0tak2.code@gmail.com>
2025-06-05 10:38:19 +0900

    feat: 디테일 뷰에서 Food를 전달받으면 연관된 Favorite을 가져와 삭제시 전달할 수 있도록 함

--- HeeBob/MenuDetail/MenuDetailDeleteButton.swift
+++ HeeBob/MenuDetail/MenuDetailDeleteButton.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct MenuDetailDeleteButton: View {
-    var food: Food
+    var favorite: Favorite
     @Environment(\.modelContext) private var modelContext
     @State private var showFavoriteDeleteAlert = false
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         Button("찜에서 삭제하기") {
@@ -18,7 +19,14 @@ struct MenuDetailDeleteButton: View {
         }
         .alert("정말 삭제 하시겠습니까?", isPresented: $showFavoriteDeleteAlert) {
             Button("삭제", role: .destructive) {
-                modelContext.delete(food)
+                modelContext.delete(favorite)
+                
+                do {
+                    try modelContext.save()
+                    dismiss()
+                } catch {
+                    print("error deleting favorite: \(error)")
+                }
             }
             Button("취소", role: .cancel) { }
         }
