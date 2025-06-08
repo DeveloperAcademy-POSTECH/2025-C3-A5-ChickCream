dec8f1a8e8505a698ce51cb15b0fbe3845014aaa
BoMin <bmlee@postech.ac.kr>
2025-06-08 14:18:05 +0900

    feat: 커스텀 내비게이션 바에 드래그 제스쳐 적용

--- HeeBob/Common/HBNavigationBar.swift
+++ HeeBob/Common/HBNavigationBar.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct HBNavigationBarModifier<L, C, R>: ViewModifier where L: View, C: View, R: View {
     @Environment(\.hbNavigationBarBackButtonHidden) private var hideBackButton
@@ -127,5 +128,17 @@ extension View {
 extension View {
     func HBNavigationBarBackButtonHidden(_ hidden: Bool) -> some View {
         environment(\.hbNavigationBarBackButtonHidden, hidden)
+    }
+}
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.isHidden = true
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
     }
 }
